3a2c3a27a5303a0c8cad01fb20e03c8ba4299bc3
enrique <enrique@keyko.io>
2021-05-21 16:12:39 +0200

    Use variable

--- scripts/register_services.sh
+++ scripts/register_services.sh
@@ -7,10 +7,10 @@ CONTROL_CENTER_URL=http://localhost:3020
 until [ $HTTP_CODE -eq 200 ] || [ $RETRY_COUNT -eq 240 ]; do
   HTTP_CODE=$(curl -s -o /dev/null -w ''%{http_code}'' $CONTROL_CENTER_URL)
   if [ $HTTP_CODE -eq 200 ]; then
-    TOKEN=$(curl -X POST "http://localhost:3020/api/auth/login" -H  "accept: text/html" -H  "Content-Type: application/x-www-form-urlencoded" -d "username=admin&password=admin")
-    curl -X POST "http://localhost:3020/api/system/service" -H  "accept: */*" -H  "auth: ${TOKEN}" -H  "Content-Type: application/x-www-form-urlencoded" -d "url=http%3A%2F%2Flocalhost%3A5000&serviceType=Metadata&description=Metadata%20tools"
-    curl -X POST "http://localhost:3020/api/system/service" -H  "accept: */*" -H  "auth: ${TOKEN}" -H  "Content-Type: application/x-www-form-urlencoded" -d "url=http%3A%2F%2Flocalhost%3A8030&serviceType=Gateway&description=Gateway%20tools"
-    curl -X POST "http://localhost:3020/api/system/service" -H  "accept: */*" -H  "auth: ${TOKEN}" -H  "Content-Type: application/x-www-form-urlencoded" -d "url=http%3A%2F%2Flocalhost%3A3001&serviceType=Faucet&description=Faucet%20tools"
+    TOKEN=$(curl -X POST "${CONTROL_CENTER_URL}/api/auth/login" -H  "accept: text/html" -H  "Content-Type: application/x-www-form-urlencoded" -d "username=admin&password=admin")
+    curl -X POST "${CONTROL_CENTER_URL}/api/system/service" -H  "accept: */*" -H  "auth: ${TOKEN}" -H  "Content-Type: application/x-www-form-urlencoded" -d "url=http%3A%2F%2Flocalhost%3A5000&serviceType=Metadata&description=Metadata%20tools"
+    curl -X POST "${CONTROL_CENTER_URL}/api/system/service" -H  "accept: */*" -H  "auth: ${TOKEN}" -H  "Content-Type: application/x-www-form-urlencoded" -d "url=http%3A%2F%2Flocalhost%3A8030&serviceType=Gateway&description=Gateway%20tools"
+    curl -X POST "${CONTROL_CENTER_URL}/api/system/service" -H  "accept: */*" -H  "auth: ${TOKEN}" -H  "Content-Type: application/x-www-form-urlencoded" -d "url=http%3A%2F%2Flocalhost%3A3001&serviceType=Faucet&description=Faucet%20tools"
     break
   fi
   printf "Waiting for control center api to be running at $CONTROL_CENTER_URL\n"
