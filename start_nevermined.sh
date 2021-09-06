b69b1ac32c7ce749d7f10d46d5183ab12a56879c
Rodolphe Marques <marques.rodolphe@gmail.com>
2021-09-06 10:27:29 +0200

    Disable arweave

--- start_nevermined.sh
+++ start_nevermined.sh
@@ -115,10 +115,10 @@ CHECK_ELASTIC_VM_COUNT=true
 
 # TODO: Disable this when work on arweave is done
 # Default external MetadataDB parameters with arweave
-export DB_MODULE_EXTERNAL="arweave"
-export DB_HOSTNAME_EXTERNAL="https://arweave.net"
-export DB_PORT_EXTERNAL="443"
-export DB_WALLET_FILE_PATH_EXTERNAL="./tests/resources/arweave-key-A1t0391IV20zpoKjhftup1ROdWBjBRGZrhx8pe55Uwc.json"
+# export DB_MODULE_EXTERNAL="arweave"
+# export DB_HOSTNAME_EXTERNAL="https://arweave.net"
+# export DB_PORT_EXTERNAL="443"
+# export DB_WALLET_FILE_PATH_EXTERNAL="./tests/resources/arweave-key-A1t0391IV20zpoKjhftup1ROdWBjBRGZrhx8pe55Uwc.json"
 
 # S3 integration
 export AWS_ACCESS_KEY="minioadmin"
